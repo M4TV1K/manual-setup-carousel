794e04c4da7c1c6a9b457bfb222328dfd6208379
m4tv1k <matvikfake@gmail.com>
2021-03-04 14:57:54 +0100

    Final configuration with webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackDeployPlugin = require('html-webpack-deploy-plugin');
+const deployConfig = new HtmlWebpackDeployPlugin({
+  packages: {
+    'react': {
+      scripts: {
+        variableName: 'React',
+        path: 'react.production.min.js',
+        cdnPath: 'umd/react.production.min.js',
+      }
+    },
+    'react-dom': {
+      scripts: {
+        variableName: 'ReactDOM',
+        path: 'react-dom.production.min.js',
+        cdnPath: 'umd/react-dom.production.min.js',
+      }
+    }
+  },
+  useCdn: true,
+  getCdnPath: (packageName, packageVersion, packagePath) => `https://unpkg.com/${packageName}@${packageVersion}/${packagePath}`
+});
+
+
 module.exports = {
   module: {
     rules: [
@@ -13,5 +37,38 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html"
+    }),
+    deployConfig
+  ]
+};
+
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html"
+    }),
+    deployConfig
+  ]
 };
